e44da1e2d2ea63dd5073d56cf5af86df0bc0a7e3
JUANITEZ123 <148462093+JUANITEZ123@users.noreply.github.com>
2023-12-05 20:16:40 -0600

    Prueba Juan

--- JuegoDeCartas/Program.cs
+++ JuegoDeCartas/Program.cs
@@ -11,6 +11,7 @@ namespace JuegoDeCartas
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World Juan");
             Console.ReadKey();
         }
     }
